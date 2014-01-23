beade9cf52666575f2c87e0391110d04472d1382
jixiangac <wb-huangjixiang@taobao.com>
2014-01-23 20:20:02 +0800

    增加回temp

---
+++ temp/compiled/cat_tit2.lbi.php
@@ -0,0 +1,18 @@
+<h2> <s></s>
+	<div class="floorTop">
+		<ul class="floorTop-nav">
+			
+	            	            <li><a href="#" target="_blank">呢大衣</a></li>
+	            	            <li><a href="#" target="_blank">羽绒</a></li>
+	            	            <li><a href="#" target="_blank">毛衣</a></li>
+	            	            <li><a href="#" target="_blank">外套</a></li>
+	            	            <li><a href="#" target="_blank">男羽绒</a></li>
+	            	            <li><a href="#" target="_blank">皮衣</a></li>
+	            	            <li><a href="#" target="_blank">男毛衣</a></li>
+	            	            <li><a href="#" target="_blank">文胸</a></li>
+	            	            <li><a href="#" target="_blank">保暖</a></li>
+	            	            <li><a style="border-right:none" href="#" target="_blank">睡衣</a></li>
+	            	        
+		</ul>
+	</div>
+	</h2>
---
+++ temp/compiled/cat_tit5.lbi.php
@@ -0,0 +1,16 @@
+<h2> <s></s>
+	<div class="floorTop">
+		<ul class="floorTop-nav">
+			
+	            	            <li><a href="#" target="_blank">茅台5折</a></li>
+	            	            <li><a href="#" target="_blank">特产专场</a></li>
+	            	            <li><a href="#" target="_blank">零食3折</a></li>
+	            	            <li><a href="#" target="_blank">洋年货</a></li>
+	            	            <li><a href="#" target="_blank">茶叶专场</a></li>
+	            	            <li><a href="#" target="_blank">年货3折</a></li>
+	            	            <li><a style="border-right:none" href="#" target="_blank">礼盒特卖</a></li>
+	            	           
+	            	        
+		</ul>
+	</div>
+	</h2>
