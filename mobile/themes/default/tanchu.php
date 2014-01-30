858d2ca5ee135fae339824cfa37404c9c301ea30
root <root@AY1311201305445223feZ.(none)>
2014-01-30 15:18:13 +0800

    底部导航

---
+++ mobile/themes/default/tanchu.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+<META content="IE=7.0000" http-equiv="X-UA-Compatible">
+<TITLE>{$page_title} 触屏版</TITLE>
+<META content="text/html; charset=utf-8" http-equiv=Content-Type>
+<META name=viewport 
+content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+<META name=apple-themes-web-app-capable content=yes>
+<META name=apple-themes-web-app-status-bar-style content=black>
+<META name=format-detection content=telephone=no>
+<LINK rel=apple-touch-icon-precomposed href="themes/default/images/touch-icon.png">
+<LINK rel="shortcut icon" type=image/x-icon href="themes/default/images/favicon2.ico">
+<LINK rel=stylesheet type=text/css href="themes/default/index.css">
+</HEAD>
+<BODY>
+  <!--搜索弹出框-->
+<DIV id=main-search class=main-search>
+	<DIV class=hd> <SPAN class=close>关闭</SPAN> </DIV>
+    <DIV class=bd>
+      <DIV class=kwd>
+        <form id="searchForm" name="searchForm" method="get" action="search.php" onSubmit="return checkSearchForm()" >
+          <DIV class=content>
+            <INPUT class=text type=search  name="keywords"   id="keyword" >
+            <SPAN class=clear>清除</SPAN>
+            <INPUT class=sub value=搜索 type=submit>
+          </DIV>
+        </FORM>
+      </DIV>
+	</DIV>
+	<P class="clear-history hidden"><A href="#">清除历史记录</A></P>
+</DIV>
+      
+</div>
+</BODY>
+</HTML>
