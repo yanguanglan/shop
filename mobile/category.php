d03866e8d4bee523fe7e8e39b4eb4c91815fffda
jixiangac <wb-huangjixiang@taobao.com>
2014-02-05 20:20:49 +0800

    注释

--- mobile/category.php
+++ mobile/category.php
@@ -325,7 +325,8 @@ if (!$smarty->is_cached('category.dwt', $cache_id))
     $position = assign_ur_here($cat_id, $brand_name);
     $smarty->assign('page_title',       $position['title']);    // 页面标题
     $smarty->assign('ur_here',          $position['ur_here']);  // 当前位置
-    $smarty->assign('categories',       get_categories_tree($cat_id)); // 分类树
+    $smarty->assign('categories',       get_categories_tree(null)); // 分类树
+    // print_r( get_categories_tree(null) );
     $smarty->assign('helps',            get_shop_help());              // 网店帮助
     $smarty->assign('top_goods',        get_top10());                  // 销售排行
     $smarty->assign('show_marketprice', $_CFG['show_marketprice']);
