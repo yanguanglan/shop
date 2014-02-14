889c6dab2b4afb06410456f5d1ab0b5b3889c602
suochao <234145193@qq.com>
2014-02-14 15:45:39 +0800

    'x'

--- mobile/return_url.php
+++ mobile/return_url.php
@@ -13,8 +13,8 @@
  * 该页面可以使用PHP开发工具调试，也可以使用写文本函数logResult，该函数已被默认关闭，见alipay_notify_class.php中的函数verifyReturn
  */
 
-require_once("create_partner_trade_by_buyer/alipay.config.php");
-require_once("create_partner_trade_by_buyer/lib/alipay_notify.class.php");
+require_once("../create_partner_trade_by_buyer/alipay.config.php");
+require_once("../create_partner_trade_by_buyer/lib/alipay_notify.class.php");
 ?>
 <!DOCTYPE HTML>
 <html>
